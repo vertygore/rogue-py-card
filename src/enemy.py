a54f59a99b9afe8649fae87b7c02df4980f5e193
{Ariond} <{d.kern.opp@gmail.com}>
2025-07-07 12:45:07 +0200

    create enemy

---
+++ src/enemy.py
@@ -0,0 +1,15 @@
+class enemy:
+    def __init__(self, name: str, hp: int, attack: int, description: str):
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+        self.description = description
+
+    def attack(self, player):
+        player.hp = player.hp - enemy.attack
+        
+    def defend(self, player):
+        self.hp = self.hp - player.attack
+        
+    def __str__(self):
+        return f"{self.name} (HP: {self.hp}, Attack: {self.attack}, Defense: {self.defense}) - {self.description}"
