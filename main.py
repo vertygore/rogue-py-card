7b333b1ca21f03fb31eaedaabd087b3bcd1bcf2f
Angelo <walburger.privat@gmail.com>
2025-07-07 08:38:40 +0200

    comment grundidee

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import tcod
 
+"""
+GAMENAME_TBD von Daniel Kern und Angelo Walburger
+
+Grundidee:
+Kartenspiel + Roguelike, angelehnt an 'Slay The Spire'
+
+Startdeck, Deck kann erweitert werden und wird stärker mit Progess
+(Am Ende) Man kann herumlaufen, Begegnungen starten Kämpfe als Kartenspiel
+
+Optionen zum upgraden von Runs:
+ - mehr Leben
+ - bessere RNG-Chancen
+ - Kartenupgrades
+ - neue Karten
+ - etc.
+
+"""
 
 def main():
     print("Hello World!")
