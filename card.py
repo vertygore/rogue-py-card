cd689baded25ff5f8c8696606de57416539b352b
{Ariond} <{d.kern.opp@gmail.com}>
2025-07-07 09:37:12 +0200

    card.py: Add new feature to enhance user experience

--- card.py
+++ card.py
@@ -1,12 +1,32 @@
-class card:
-    def __init__(self, name: str, cost: int, hp: int, damage:int, description: str,type: str):
+
+from abc import ABC, abstractmethod
+
+
+class Card(ABC):
+    def __init__(self, name: str, cost: int, description: str):
         self.name = name
         self.cost = cost
-        self.hp = hp
-        self.damage = damage
         self.description = description
-        self.type = type
-
+        
+class offspell(Card):
+        def __init__(self, damage: int):
+            super().__init__(name, cost, description)
+            self.damage = damage
+        
+class defensespell(Card):
+        def __init__(self, heal: int):
+            super().__init__(name, cost, description)
+            self.heal = heal
+            
+class potion (Card):
+        def __init__(self, heal: int):
+            super().__init__(name, cost, description)
+            self.heal = heal
+class weapon(Card):
+    def __init__(self, damage: int):
+        super().__init__(name, cost, description)
+        self.damage = damage
+       
     def __repr__(self):
         return f"Card(name={self.name}, cost={self.cost}, description={self.description})"
 
